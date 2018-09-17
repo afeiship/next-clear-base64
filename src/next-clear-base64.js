49544caca87a6ff672f929cc1ef184c416e5c765
afeiship <1290657123@qq.com>
2018-09-17 09:26:05 +0800

    issue: opmtize code

--- src/next-clear-base64.js
+++ src/next-clear-base64.js
@@ -2,9 +2,10 @@
 
   var global = global || this || window || Function('return this')();
   var nx = global.nx || require('next-js-core2');
+  var CHAR_COMMA = ',';
 
   nx.clearBase64 = function (inValue) {
-    var index = inValue.indexOf(',');
+    var index = inValue.indexOf(CHAR_COMMA);
     return inValue.substr(index + 1);
   };
 
